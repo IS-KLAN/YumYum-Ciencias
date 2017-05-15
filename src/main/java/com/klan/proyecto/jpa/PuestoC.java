455b486b7cc7a0f7589e40d15d63131286c9eaeb
kcontrerasm <karlaaw.kc@gmail.com>
2017-05-15 14:49:28 -0500

    Agrego comentarios a la PuestoC

--- src/main/java/com/klan/proyecto/jpa/PuestoC.java
+++ src/main/java/com/klan/proyecto/jpa/PuestoC.java
@@ -22,39 +22,44 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 /**
+ *Clase JPA para Crear, Modificar, y eliminar un puesto de la BD.
  *
  * @author patlani
  */
 public class PuestoC implements Serializable {
 
     /**
-     * 
-     * @param emf 
+     * Constructor de la clase JPA para un Puesto.
+     * @param em  Objeto de EntityManagerFactory encargado de la conexión
+     * a la BD.
      */
-    public PuestoC(EntityManagerFactory emf) {
-        this.emf = emf;
+    public PuestoC(EntityManagerFactory em) {
+    	this.emf = em;
     }
-    
+
     /**
-     * 
+     * Objeto de EntityManagerFactory encargado de la conexión a la BD.
      */
     private EntityManagerFactory emf = null;
 
     /**
-     * 
-     * @return 
+     * Método encargado de comenzar la conexión con la BD.
+     * @return Objeto creado ara la conexión a la BD.
      */
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
 
     /**
-     * 
-     * @param puesto
-     * @throws EntidadExistenteException
-     * @throws Exception 
+     * Metodo encargado de la creacion de un puesto en la BD.
+     * @param puesto	El puesto que se guardara en la BD
+     * @throws EntidadExistenteException excepcion lanzada  cuando
+     * ya existe un puesto guardado en la BD con el mismo nombre
+     * @throws Exception Excepcion lanzanda si se presenta un
+     * error al guardar el puesto en la base de datos.
      */
-    public void crear(Puesto puesto) throws EntidadExistenteException, Exception {
+    public void crear(Puesto puesto) 
+    					throws EntidadExistenteException, Exception {
         if (puesto.getComidas() == null) {
             puesto.setComidas(new ArrayList<Comida>());
         }
@@ -67,13 +72,15 @@ public class PuestoC implements Serializable {
             em.getTransaction().begin();
             List<Comida> comidas = new ArrayList<Comida>();
             for (Comida comida : puesto.getComidas()) {
-                comida = em.getReference(comida.getClass(), comida.getNombre());
+                comida = em.getReference(comida.getClass(),
+										comida.getNombre());
                 comidas.add(comida);
             }
             puesto.setComidas(comidas);
             List<Evaluacion> evaluaciones = new ArrayList<Evaluacion>();
             for (Evaluacion evaluacion : puesto.getEvaluaciones()) {
-                evaluacion = em.getReference(evaluacion.getClass(), evaluacion.getLlave());
+                evaluacion = em.getReference(evaluacion.getClass(),
+                							 evaluacion.getLlave());
                 evaluaciones.add(evaluacion);
             }
             puesto.setEvaluaciones(evaluaciones);
@@ -94,7 +101,8 @@ public class PuestoC implements Serializable {
             em.getTransaction().commit();
         } catch (Exception ex) {
             if (buscaNombre(puesto.getNombre()) != null) {
-                throw new EntidadExistenteException("Puesto " + puesto + " ya existe.", ex);
+                throw new EntidadExistenteException("Puesto " + puesto
+                	+ " ya existe.", ex);
             }
             throw ex;
         } finally {
@@ -105,14 +113,18 @@ public class PuestoC implements Serializable {
     }
 
     /**
-     * 
-     * @param puesto
-     * @throws InconsistenciasException
-     * @throws EntidadInexistenteException
-     * @throws Exception 
+     * Metodo que se encarga de editar un puesto guardado en la BD.
+     * @param puesto	El puesto a editar en la BD.
+     * @throws InconsistenciasException	Excepcion lanzada al encontrar
+     * datos que no corresponden con los anteriores.
+     * @throws EntidadInexistenteException Excepcion lanzada al querer
+     * editar un puesto que no existe en la BD
+     * @throws Exception Excepcion lanzada al presentarse un error en la
+     * edicion de un puesto de la BD.
      */
     public void editar(Puesto puesto)
-                  throws InconsistenciasException, EntidadInexistenteException, Exception {
+                  throws InconsistenciasException, EntidadInexistenteException,
+                  													Exception {
         EntityManager em = null;
         try {
             em = getEntityManager();
@@ -128,7 +140,8 @@ public class PuestoC implements Serializable {
                     if (illegalOrphanMessages == null) {
                         illegalOrphanMessages = new ArrayList<String>();
                     }
-                    illegalOrphanMessages.add("Debe conservarse Evaluacion " + evaluacion);
+                    illegalOrphanMessages.add("Debe conservarse Evaluacion "
+                    	+ evaluacion);
                 }
             }
             if (illegalOrphanMessages != null) {
@@ -143,7 +156,8 @@ public class PuestoC implements Serializable {
             puesto.setComidas(comidasNuevas);
             List<Evaluacion> evaluacionesNuevas = new ArrayList<Evaluacion>();
             for (Evaluacion evaluacion : evaluacionesNew) {
-                evaluacion = em.getReference(evaluacion.getClass(), evaluacion.getLlave());
+                evaluacion = em.getReference(evaluacion.getClass(),
+                										 evaluacion.getLlave());
                 evaluacionesNuevas.add(evaluacion);
             }
             evaluacionesNew = evaluacionesNuevas;
@@ -178,7 +192,8 @@ public class PuestoC implements Serializable {
             if (msg == null || msg.length() == 0) {
                 String id = puesto.getNombre();
                 if (buscaNombre(id) == null) {
-                    throw new EntidadInexistenteException("No existe puesto con id " + id);
+                   throw new
+                   EntidadInexistenteException("No existe puesto con id " + id);
                 }
             }
             throw ex;
@@ -190,10 +205,13 @@ public class PuestoC implements Serializable {
     }
 
     /**
-     * 
-     * @param nombre
-     * @throws InconsistenciasException
-     * @throws EntidadInexistenteException 
+     * Metodo que se encarga de borrar un Puesto de la BD a partir del
+     * nombre del puesto.
+     * @param nombre El nombre del puesto que se quiere borrar.
+     * @throws InconsistenciasException	Excepcion lanzada al intentar borrar
+     * un puesto sin haber eliminado las evaluaciones.
+     * @throws EntidadInexistenteException Excepcion lanzada al intentar
+     * borrar un puesto de la BD que no se encuentra guardada.
      */
     public void borrar(String nombre)
                   throws InconsistenciasException, EntidadInexistenteException {
@@ -206,7 +224,8 @@ public class PuestoC implements Serializable {
                 puesto = em.getReference(Puesto.class, nombre);
                 puesto.getNombre();
             } catch (EntityNotFoundException enfe) {
-                throw new EntidadInexistenteException("No existe puesto con id " + nombre, enfe);
+                throw new EntidadInexistenteException("No existe puesto con id "
+                											 + nombre, enfe);
             }
             List<String> mensajes = null;
             List<Evaluacion> consistencias = puesto.getEvaluaciones();
@@ -215,7 +234,8 @@ public class PuestoC implements Serializable {
                     mensajes = new ArrayList<String>();
                 }
                 mensajes.add("Puesto (" + puesto
-                + ") no puede destruirse porque no existe Evaluacion " + evaluacion);
+                + ") no puede destruirse porque no existe Evaluacion "
+                										+ evaluacion);
             }
             if (mensajes != null) {
                 throw new InconsistenciasException(mensajes);
@@ -235,31 +255,33 @@ public class PuestoC implements Serializable {
     }
 
     /**
-     * 
-     * @return 
+     * Metodo que recupera todos los puestos de la base de datos.
+     * @return Una llamada al metodo buscaPuestos.
      */
     public List<Puesto> buscaPuestos() {
         return buscaPuestos(true, -1, -1);
     }
 
     /**
-     * 
+     * Metodo que recupera los puestos.
      * @param maxResults
      * @param firstResult
-     * @return 
+     * @return una llamada al metodo buscaPuestos.
      */
     public List<Puesto> buscaPuestos(int maxResults, int firstResult) {
         return buscaPuestos(false, maxResults, firstResult);
     }
 
     /**
-     * 
+     * Metodo que recupera la lista de todos los puestos registrados
+     * en la BD
      * @param all
      * @param maxResults
      * @param firstResult
-     * @return 
+     * @return una lista con todos los puestos de la BD
      */
-    private List<Puesto> buscaPuestos(boolean all, int maxResults, int firstResult) {
+    private List<Puesto> buscaPuestos(boolean all, int maxResults,
+    											   int firstResult) {
         EntityManager em = getEntityManager();
         try {
             CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
@@ -276,9 +298,9 @@ public class PuestoC implements Serializable {
     }
 
     /**
-     * 
-     * @param nombre
-     * @return 
+     * Metodo que realiza una busqueda entre los puestos por el nombre.
+     * @param nombre El nombre del puesto que se quiere buscar.
+     * @return El puesto con el nombre que le pasamos como parametro.
      */
     public Puesto buscaNombre(String nombre) {
         EntityManager em = getEntityManager();
@@ -290,26 +312,30 @@ public class PuestoC implements Serializable {
     }
 
     /**
-     * 
-     * @param latitud
-     * @param longitud
-     * @return 
+     * Metodo que realiza una busqueda entre los puestos por su ubicacion.
+     * @param latitud La latitud que corresponde al puesto que necesitamos
+     * buscar.
+     * @param longitud La longitud que corresponde al puesto que necesitamos
+     * buscar.
+     * @return El puesto que se encuentra ubicado en la latitud y longitud
+     * que pasamos como parametro.
      */
     public Puesto buscaLugar(String latitud, String longitud) {
-        try{
+        try {
             EntityManager em = getEntityManager();
-            return (Puesto)(em.createNamedQuery("Puesto.buscaLugar")
+            return (Puesto) (em.createNamedQuery("Puesto.buscaLugar")
                     .setParameter("latitud", latitud)
                     .setParameter("longitud", longitud).getSingleResult());
-        }catch(Exception ex){
+        } catch(Exception ex){
             System.err.println("Error al buscar el usuario con correo: "
                                                 + latitud + ex.getMessage());
-        } return null;
-    }    
+        } 
+        return null;
+    }  
 
     /**
-     * 
-     * @return 
+     * Metodo que se encarga de contar todos los puestos registrados en la BD.
+     * @return La cantidad de puestos que tenemos registrados en la BD.
      */
     public int cantidadDePuestos() {
         EntityManager em = getEntityManager();
@@ -323,5 +349,4 @@ public class PuestoC implements Serializable {
             em.close();
         }
     }
-    
 }
